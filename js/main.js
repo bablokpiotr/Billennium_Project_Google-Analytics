1e4840ddf3431a4ce5823980f16def0ff5299ec0
Piotr Bablok <bablok.piotr@gmail.com>
2021-11-25 12:19:25 +0100

    add moda login/register. CSS style

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
-"use strict";
-class RotateLogo {
-    constructor() {
-        this.tigerSpinStart = document.getElementById('tiger-spin');
-        this.tigerSticker = document.getElementById('tiger-sticker');
-        this.configure();
-    }
-    configure() {
-        this.tigerSpinStart.addEventListener('click', this.rotateLogoHandler.bind(this));
-    }
-    rotateLogoHandler() {
-        const style = window.getComputedStyle(this.tigerSticker);
-        const visibility = style.getPropertyValue('visibility');
-        if (visibility === 'hidden') {
-            this.tigerSticker.style.visibility = 'visible';
-            this.tigerSticker.style.transitionDuration = '1s';
-            this.tigerSticker.style.transform = 'rotate(360deg)';
-        }
-        else {
-            this.tigerSticker.style.transitionDuration = 'unset';
-            this.tigerSticker.style.transform = 'none';
-            this.tigerSticker.style.visibility = 'hidden';
-        }
+const modalLogin = document.getElementById('modal-login');
+const modalRegister = document.getElementById('modal-register');
+
+modalLogin.addEventListener('click', displayHandler);
+modalRegister.addEventListener('click', displayHandler);
+
+function displayHandler(e) {
+
+    if (e.path[1].id === "modal-login") {
+        setDisplay("login-display")
+    } else {
+        setDisplay("register-display")
     }
 }
-const rotateLogo = new RotateLogo();
+
+function setDisplay(id) {
+    const modal = document.getElementById(id)
+    const style = window.getComputedStyle(modal);
+    const display = style.getPropertyValue('display');
+
+    if (display === 'none') {
+        modal.style.display = 'flex';
+    } else {
+        modal.style.display = 'none';
+    }
+}
