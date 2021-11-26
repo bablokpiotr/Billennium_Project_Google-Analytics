68e4423c9be2a38595ab78e7b4ba610f66236cdd
Piotr Bablok <bablok.piotr@gmail.com>
2021-11-26 11:12:32 +0100

    partial js code to module

---
+++ js/modules/ModalLoginRegister.js
@@ -0,0 +1,26 @@
+export const modalLogin = document.getElementById('modal-login');
+export const modalRegister = document.getElementById('modal-register');
+
+modalLogin.addEventListener('click', displayHandler);
+modalRegister.addEventListener('click', displayHandler);
+
+function displayHandler(e) {
+
+    if (e.path[1].id === "modal-login") {
+        setDisplay("login-display")
+    } else {
+        setDisplay("register-display")
+    }
+}
+
+function setDisplay(id) {
+    const modal = document.getElementById(id)
+    const style = window.getComputedStyle(modal);
+    const display = style.getPropertyValue('display');
+
+    if (display === 'none') {
+        modal.style.display = 'flex';
+    } else {
+        modal.style.display = 'none';
+    }
+}
